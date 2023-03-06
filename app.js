b619774cb33411deab5c7afde3b9d271cd223d08
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-03-06 20:21:01 +0500

    Practice - swap keys and values

--- app.js
+++ app.js
@@ -1,25 +1,16 @@
 'use strict';
 
 /*
-    Итерации по Map  
+    Необходимо поменять местави ключи и значения в следующем Map 
 
 */
 
-const wetherMap = new Map([
+let wetherMap = new Map([
   ['London', 10],
   ['Moscow', 7],
   ['Paris', 14]
 ]);
 
-for (const [key, value] of wetherMap) {
-  
-}
+ wetherMap = new Map([...wetherMap].map(el => el.reverse()))
+ console.log(wetherMap);
 
-//Конвертация Map обратно в массыви
-
-console.log([...wetherMap]); // (3) [Array(2), Array(2), Array(2)];
-console.log(wetherMap.keys()); // MapIterator {'London', 'Moscow', 'Paris'}
-console.log(wetherMap.values()); // MapIterator {10, 7, 14}
-
-console.log([...wetherMap.keys()]); //  ['London', 'Moscow', 'Paris']
-console.log([...wetherMap.values()]); // [10, 7, 14]
