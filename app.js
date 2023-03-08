9b8184d87841feef7f400fa00862d3c5da403c73
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-03-08 21:33:17 +0500

    Остаток от деления

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 'use strict';
 
 /*
-    Упражнение - Функция случайного числа
+    Оператор остатка от деления
 
 */
-function randomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min)
-}
 
-console.log(randomNumber(1, 20));
+console.log(15 / 2); //7.5
+console.log(15 % 2); //14 / 2 + 1 = 15 ( 1-остаток)
+console.log(14 % 2); //0
+
+const isEven = n => n % 2 === 0;
+const isOdd = n => n % 2 === 1;
+function isEvenFunc(n) {
+    return n % 2 === 0
+}
+console.log(isEven(15)); //false
+console.log(isEven(14)); //true
+
+console.log(isOdd(15));//true
+console.log(isOdd(14));//false
