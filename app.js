ed3969cb0f6be4c4d350c21d03482c01873b0a24
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-03-08 21:27:20 +0500

    Practice - function random number

--- app.js
+++ app.js
@@ -1,32 +1,11 @@
 'use strict';
 
 /*
-    Округление
+    Упражнение - Функция случайного числа
 
 */
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
 
-/* Округление числа до ближвйщего целого */
-console.log(Math.round(1.5));// 2
-console.log(Math.round(1.6));// 2
-console.log(Math.round(1.4));// 1
-console.log(Math.round(1.3));// 1
-
-/* Округление до верхнего значения */
-console.log(Math.ceil(1.1));//2
-console.log(Math.ceil(1.3));//2
-console.log(Math.ceil(1.7));//2
-console.log(Math.ceil(0.7));//1
-
-/* Округления до нижнего значения */
-console.log(Math.floor(1.1));//1
-console.log(Math.floor(1.9));//1
-console.log(Math.floor(0.9));//0
-
-/* Округления число до целого */
-console.log(Math.trunc(2.9));//2
-console.log(Math.trunc(2.1));//2
-console.log(Math.trunc(1.1111111));//1
-
-/* Округление до определенного знака. ВОЗВРАЩАЕТ СТРОКУ */
-console.log(1.49999.toFixed(1));// 1.5 - string
-console.log(Number(1.49999.toFixed(1)));// 1.5 - number
+console.log(randomNumber(1, 20));
