f14aeaf536e00fa1ec21bda4957aea90bf6248cc
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-03-09 22:54:03 +0500

    Сравнение дат

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 'use strict';
 
 /*
-   операции с датами
+   Сравнение дат
 
 */
 
-const date1 = new Date(2024, 10, 15);
-const date2 = new Date(2025, 11, 15);
-console.log(Number(date1));// тот же time stempt 1731610800000
-console.log(Number(date2));//1734202800000
-console.log(date2 - date1);//2592000000
+const first = new Date(2024, 10, 4);
+const second = new Date(2024, 10, 3);
+
+/* Под капотом JS сравнивает Time Stempt ( миллисекунды) */
+console.log(first > second);//true
+console.log(first < second); // false
 
 
-function getDaysBetweenDates(dateFirst, dateSecond) {
-    return (dateSecond - dateFirst) / (1000 * 60 * 60 * 24)
-}
-console.log(getDaysBetweenDates(date1, date2));
+
+/* В данном случае он сравнивает ссылки на объект, а они разные поэтому false */
+const first1 = new Date(2024, 10, 4);
+const second1 = new Date(2024, 10, 4);
+
+console.log(first1 == second1);//false
+console.log(first1 === second1);//false
+
+/* Как же тогда сравнить */
+console.log(first1.getTime() == second1.getTime());//true
+console.log(first1.getTime() === second1.getTime());//true
+console.log(Number(first1) === Number(second1));//true
+
