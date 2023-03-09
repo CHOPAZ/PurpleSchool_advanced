d48c953912f2b3663b9f3f3f5a3110b50da004f6
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-03-09 21:21:40 +0500

    Practice - Конвертер валют

--- app.js
+++ app.js
@@ -1,37 +1,49 @@
 'use strict';
 
 /*
-  Интернационализация чисел
+    Напишите функцию, которая принимает 3 параметра:
+    - Сумма
+    - Валюта исходная
+    - Валюта для конвертации
+    И возвращает строку уже сконвертированной суммы с постфиксом валюты. Если не смог, то null
+    Для примера 3 валюты.
 
 */
 
-console.log(23000);//23000
+function conversionCurrencies(sum, initialCurrency, convertCurrency) {
+    const allCurrencies = [
+        {
+            name: 'USD',
+            mult: 1
+        },
+        {
+            name: 'RUB',
+            mult: 1 / 60
+        },
+        {
+            name: 'EUR',
+            mult: 1.1
+        }
+    ];
+    const initial = allCurrencies.find(c => c.name === initialCurrency);
+    if (!initial) {
+        return null
+    }
 
-const options = {
-    style: 'currency',
-    currency: 'RUB'
+    const convert= allCurrencies.find(c => c.name === convertCurrency);
+    if (!initial) {
+        return null;
+    }
+
+    return  new Intl
+        .NumberFormat('ru-RU', {style: 'currency', currency: convert.name})
+        .format(sum * initial.mult / convert.mult)
+
+
 }
 
-const options2 = {
-    style: 'currency',
-    currency: 'USD'
-}
+console.log(conversionCurrencies(10000, 'RUB', 'USD'));//166,67 $
+console.log(conversionCurrencies(10000, 'RUB', 'EUR'));//151,52 €
+console.log(conversionCurrencies(10000, 'EUR', 'USD'));//11 000,00 $
 
-const options3 = {
-    style: 'decimal'
-}
 
-const options4 = {
-    style: 'percent'
-}
-
-const options5 = {
-    style: 'unit',
-    unit: 'celsius'
-}
-
-console.log(new Intl.NumberFormat('ru-RU', options).format(23000));//23 000,00 ₽
-console.log(new Intl.NumberFormat('en-US', options2).format(23000));//$23,000.00
-console.log(new Intl.NumberFormat('ru-RU', options3).format(23000));//23 000
-console.log(new Intl.NumberFormat('ru-RU', options4).format(0.1));//10%
-console.log(new Intl.NumberFormat('ru-RU', options5).format(25));//25 °C
