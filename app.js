29dc50d11d889fdc46cabab444bd29506e7f91fb
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-03-09 20:43:47 +0500

    BigInt

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
 'use strict';
 
 /*
-    Разделитель чисел
+   Работа с BigInt
 
 */
 
-const bigNum1 = 350500000;
-const bigNum2 = 350_500_000;
+/* Проблема, если число больше 9007199254740991, то работа с ним считается не безапасной(SAFE) */
+console.log(Number.MAX_SAFE_INTEGER);//9007199254740991 - максимальное доступное число для чисел
+console.log(Number.MIN_SAFE_INTEGER);// -9007199254740991 - 
+console.log(2**53 - 1);// 9007199254740991
 
-const payment = 20_10;
-const payment2 = 2_0.1_0;
+/* BigInt - позволяет работать с числами больше чем 9007199254740991 */
 
-console.log(Number('350_500_000'));//NaN
-console.log(Number('350500000'));//350500000
-console.log(Number.parseInt('350_500_000'))//350 - некоректно
-console.log(Number.parseFloat('350_500_000'))//350 - некоректно
+console.log(12312312312312312312321123n);//12312312312312312312321123n
+console.log(BigInt(12312312312312312312321123));//12312312312312311989665792n
+console.log(BigInt('12312312312312312312321123'));//12312312312312312312321123n
+
+console.log(10n + 10n);//20n
+// console.log(10n + 10);//Ошибка   Cannot mix BigInt and other types, use explicit conversions
+console.log(10n * 10n);// 100n;
+
+console.log(10n * BigInt(10));//100n
+
+console.log(10n < 20);//true
+console.log(10n == 10);//true
+console.log(10n === 10);//false
+
+console.log(10n / 3n);//3n
+console.log(10 / 3);//3.3333333333335
+
+/* BigInt не может иметь плавущую точку */
