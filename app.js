7e527eb676e24ff3f46bd9d652d7043511b98d93
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-03-11 23:06:31 +0500

    Practice - Timer pizza

--- app.js
+++ app.js
@@ -1,20 +1,37 @@
 'use strict';
 
 /*
-    работы с интервалами
+    Упражнение - таймер пиццы
+
+    Сделать таймер пиццы (функцию, принимающую время)б
+    который будет выводить в консоль секунды, оставшиеся до готовнисти пиццы и сообщение по готовнисти
+    00:04
+    00:03
+    00:02
+    00:01
+    Готово!!!
+
 
 */
 
-const interval = setInterval(() => {
-    console.log(new Date());
-}, 1000)
-
-const timer = setTimeout(() => {
-    clearInterval(interval)
-}, 5000)
-
-console.log(interval);
-console.log(timer);
 
 
+function timerPizza(ms) {
+    const end = new Date().getTime() + ms;
+    const interval = setInterval(() => {
+        console.log(
+            new Intl.DateTimeFormat('ru-RU',{
+                minute: 'numeric',
+                second: 'numeric'
+            }).format(end + 100 - new Date())
+        );
+    }, 1000);
+
+    setTimeout(() => {
+        clearInterval(interval)
+        console.log('Pizza is ready!!');
+    }, ms)
+}
+
+timerPizza(5000)
 
