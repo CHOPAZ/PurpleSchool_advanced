beb5dfcd8786833b904025a185f08646ead1dd64
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-03-12 23:00:04 +0500

    new

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
 'use strict';
 
 /*
-    Реализация ООП в JS - с помощью насл едования прототипов.
-    Объекты связаны с прототипами объектам. Не копирует, а имеет доступ
+    Оператор new
+    constructor function
+
+    Когда мы пишем new у нас происходят несколько шаго:
+
+    1. Создается пустой объект
+    2. Вызывается функция конструктор User = function(email, password) {}
+    3.  this.email = email -  email: "a@a.ru"
+    4. Созданный объект связывается с prototype
+    5. Возвращается объект автоматически
 */
 
-const a = [1];
-console.dir(a)
+/* Функция конструктор User */
 
-/* Как создать
-    1. Constructor functions
-    2. ES6 Classes
-    3. Object.create()
-*/
+const User = function(email, password) {
+    this.email = email;
+    this.password = password
+}
+
+const user1 = new User('a@a.ru', '123')
+const user2 = new User('b@b.ru', '1233')
+
+console.log(user1);
+console.log(user2);
+
+/* Сравнить что User2 является инстанс этого пользователя */
+
+console.log(user2 instanceof User); // true
