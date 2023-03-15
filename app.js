9c9e2a22f5feb9442c4e6709e27d5669ed4ff74d
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-03-15 21:29:58 +0500

    Абстракция и инкапсуляция

--- app.js
+++ app.js
@@ -1,38 +1,52 @@
 'use strict'
 
 /*
-  Object.create
+  Принципы ООП в классах
+
+  Абстракция и инкапсуляция
 */
 
-const User = {
-  init(email, password) {
-    this.email1 = email
-    this.password1 = password
-  },
+/* Примерные свойства фильма
+  - Название
+  - Режиссер
+  - Рейтинг
+  - Длительность
+  - Страна производителя
+  - Актеры
+  - Трейлер
+  ...
 
-  log() {
-    console.log('log')
-  },
+  В первую очередь нужно понять что из объекта будем использовать.
+
+  В кажестве АБСТРАКЦИИ мы должны понять что объект, который мы будем проектировать, он будет отличаться от реального объекта.
+*/
+
+class Film {
+  #name
+  #author
+  rating
+  #length
+
+  constructor(name, author, length) {
+    this.#name = name
+    this.#author = author
+    this.#length = length
+  }
+
+  get name() {
+    return this.#name
+  }
+
+  get author() {
+    return this.#author
+  }
+
+  get length() {
+    return this.#length
+  }
 }
-const user = Object.create(User)
-console.log(user)
-console.log(user.__proto__ === User) //true
-user.log()
 
-/* На практике это используется не часто, потому что что нам нужно что бы задать у пользователя логин и пароль  */
+/* Инкапсуляция - скрытие свойст, доступн снаруже запрещен */
 
-user.email = 'a@aa.ru'
-user.password = '123'
-/* Для избежания этого часто используется хак инит */
-user.init('bbb@b.ru', '121')
-
-/* Самое важное, что с помощью Object.create() мы создали вручную цепочку прототипов */
-
-const admin = Object.create(user);
-console.log(admin);
-
-/* Мы создали админ, в прототипе у него user, в прототипе у пользователя объект с методами log и init, в прототипе объекта сам корневой объект
-
-admin будет доступен метод log, потому что он унаследовался у user
-*/
-admin.log()
+const avatarFilm = new Film('Avatar', 'Cameron', 240)
+console.log(avatarFilm)
