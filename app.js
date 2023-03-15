c57e8440783f9e8c50d6efd8e658f25f3da5acf3
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-03-15 19:04:32 +0500

    Static

--- app.js
+++ app.js
@@ -1,66 +1,70 @@
 "use strict";
 
 /*
-  Setters and getters - возможность использовать свойства как методы, вызывая их как свойства
-  getter - позволяет получить какое-то значение, обращаясь к нему как к свойству.
-  setter - позволяет установить какое-то значение. Set должен принимать одно значение то чему должно быть присвоено.
-  По сути это некоторое скрытие за собой реализации получения тех или иных данных
+  Static. Статичные свойства
 */
 
-const task = {
-  title: "Task1",
-  dueTo: new Date("2023/01/01"),
+/* Как они записываются и работают под капотом */
 
-  /* Обычным способом. Проверка на истекшее время */
-  // isOverdue() {
-  //   return this.dueTo < new Date();
-  // },
+/* Как раньше можно было сделать */
 
-  /* Переделаем на get */
-  get isOverdue() {
-    return this.dueTo < new Date();
-  },
-
-  /* Установим свойство */
-  set isOverdue(isOverdueTask) {
-    if (!isOverdueTask) {
-      this.dueTo = new Date();
-    }
-  },
-};
-
-/* И вызывали бы как: */
-// console.log(task.isOverdue()); // true
-
-/* Теперь нужно обратится к нему как к свойству */
-console.log(task.isOverdue); //true
-task.isOverdue = false;
-console.log(task); // поменяется dueTo
-
-/* Теперь рассмотрим на классах */
-
-class Task {
-  constructor(title, dueDate) {
-    this.title = title;
-    this.dueDate = dueDate;
-  }
-
-  get isOverdue() {
-    return this.dueDate < new Date();
-  }
-
-  /* Сделаем валидацию установки dueDate */
-
-  set dueDate(date) {
-    if (date < new Date()) {
-      return;
-    }
-    // this.dueDate = date; // работать не будет, потому что будем пытатся вызывать рекурсивно наш сеттер. call stack будет переполнен. Обычно в классах используются либо приватные свойства либо свойства с нижнем подчеркиванием
-    this._dueDate = date;
+class Test {
+  hello() {
+    console.log("Hello");
   }
 }
 
-const newTask = new Task("Task2", new Date());
-console.log(newTask.isOverdue); // false
-console.log((newTask.dueDate = new Date("2024/1/1")));
-console.log(newTask);
+const test = new Test();
+test.hello();
+
+/* Без инициализации конкретного инстанса, а просто как грубог оворя нейм спейс, в котором находится этот способ или свойство. Т.е мы не создаем const test = new Test(); */
+
+class Test1 {
+  static hello() {
+    console.log("Hello");
+  }
+}
+
+Test1.hello()
+
+/* Как работает под капотом */
+
+const Test2 = function() {
+
+}
+
+Test2.hello = function() {
+  console.log('Hello');
+}
+
+
+Test2.hello()
+
+/* При этом могут быть не только статичными методы, но и свойства */
+
+class Test3 {
+  static a = 1;
+  static hello() {
+    console.log("Hello");
+  }
+}
+
+console.log(Test3.a);
+
+/* Самое интересное что могут быть статичные блоки, которые позволяют улучшить инициализацию статичных свойст */
+
+class Test4 {
+  static a = 1;
+  static hello() {
+    console.log("Hello");
+  }
+  static {
+    let b = 5
+    this.a = 5
+  }
+}
+console.log(Test4.a);
+
+/* Когда это можно использовать
+  Добавить набор методов, свойст, без необходимости напрямую создания его инстанса
+*/
