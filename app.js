8db7caf936c2aa2cdb63e4ec35290b872bfca2b7
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-03-16 21:06:08 +0500

    Наследование

--- app.js
+++ app.js
@@ -1,52 +1,62 @@
-'use strict'
+'use strict';
 
 /*
   Принципы ООП в классах
 
-  Абстракция и инкапсуляция
+  Наследование. Через construction function
 */
 
-/* Примерные свойства фильма
-  - Название
-  - Режиссер
-  - Рейтинг
-  - Длительность
-  - Страна производителя
-  - Актеры
-  - Трейлер
-  ...
+/* Как бы делали реализацию книг без наследования, код повторяетяс DRY */
+const Book = function (title, author) {
+  this.title = title;
+  this.author = author;
+};
 
-  В первую очередь нужно понять что из объекта будем использовать.
+Book.prototype.buy = function () {
+  console.log('Buy');
+};
 
-  В кажестве АБСТРАКЦИИ мы должны понять что объект, который мы будем проектировать, он будет отличаться от реального объекта.
+const AudioBook = function (title, author, lenMin) {
+  this.title = title;
+  this.author = author;
+  this.lenMin = lenMin;
+};
+
+AudioBook.prototype.log = function () {
+  console.log(`${this.name} - ${this.lenMin}`);
+};
+
+/* Переиспользование класса - еще не наследование, не все будет работать */
+
+const Book1 = function (title, author) {
+  this.title = title;
+  this.author = author;
+};
+
+Book1.prototype.buy = function () {
+  console.log('Buy');
+};
+
+const AudioBook1 = function (title, author, lenMin) {
+  /* Если просто вызовем Book(title,author), то this будет undefined, поэтому через call передаем контекс AudioBook1 */
+  Book1.call(this, title, author);
+  this.lenMin = lenMin;
+};
+
+/* Полноценное связывание контекста 
+  Именно в такой последовательности. Создаем пустой объект через Object.create, и потом его дополняем контруктором и методами
 */
+AudioBook1.prototype = Object.create(Book1.prototype);
+AudioBook1.prototype.constructor = AudioBook1;
+AudioBook1.prototype.log = function () {
+  console.log(`${this.title} - ${this.lenMin}`);
+};
 
-class Film {
-  #name
-  #author
-  rating
-  #length
+const book = new AudioBook1('Lord Of The Rings', 'Tolkein', 20 * 60);
 
-  constructor(name, author, length) {
-    this.#name = name
-    this.#author = author
-    this.#length = length
-  }
+book.log(); // Lord Of The Rings - 1200
+book.buy(); //Buy
 
-  get name() {
-    return this.#name
-  }
-
-  get author() {
-    return this.#author
-  }
-
-  get length() {
-    return this.#length
-  }
-}
-
-/* Инкапсуляция - скрытие свойст, доступн снаруже запрещен */
-
-const avatarFilm = new Film('Avatar', 'Cameron', 240)
-console.log(avatarFilm)
+console.log(book);
+console.log(book instanceof AudioBook1); // true
+console.log(book instanceof Book1); // true
