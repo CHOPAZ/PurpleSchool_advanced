b0a22f01d538848182ad1e5d5c834ef4526ff8b9
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-03-16 21:27:40 +0500

    Наследование в ES6

--- app.js
+++ app.js
@@ -3,60 +3,34 @@
 /*
   Принципы ООП в классах
 
-  Наследование. Через construction function
+  Наследование. в ES6
 */
 
-/* Как бы делали реализацию книг без наследования, код повторяетяс DRY */
-const Book = function (title, author) {
-  this.title = title;
-  this.author = author;
-};
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
 
-Book.prototype.buy = function () {
-  console.log('Buy');
-};
+  buy() {
+    console.log('Buy');
+  }
+}
 
-const AudioBook = function (title, author, lenMin) {
-  this.title = title;
-  this.author = author;
-  this.lenMin = lenMin;
-};
-
-AudioBook.prototype.log = function () {
-  console.log(`${this.name} - ${this.lenMin}`);
-};
-
-/* Переиспользование класса - еще не наследование, не все будет работать */
-
-const Book1 = function (title, author) {
-  this.title = title;
-  this.author = author;
-};
-
-Book1.prototype.buy = function () {
-  console.log('Buy');
-};
-
-const AudioBook1 = function (title, author, lenMin) {
-  /* Если просто вызовем Book(title,author), то this будет undefined, поэтому через call передаем контекс AudioBook1 */
-  Book1.call(this, title, author);
-  this.lenMin = lenMin;
-};
-
-/* Полноценное связывание контекста 
-  Именно в такой последовательности. Создаем пустой объект через Object.create, и потом его дополняем контруктором и методами
+/* extends - расширять. С помощью него мы связали прототип Book c AudioBook 
+ И еще необходимо прокинуть конструктор через super
 */
-AudioBook1.prototype = Object.create(Book1.prototype);
-AudioBook1.prototype.constructor = AudioBook1;
-AudioBook1.prototype.log = function () {
-  console.log(`${this.title} - ${this.lenMin}`);
-};
+class AudioBook extends Book {
+  constructor(title, author, lenMin) {
+    super(title, author);
+    this.lenMin = lenMin;
+  }
 
-const book = new AudioBook1('Lord Of The Rings', 'Tolkein', 20 * 60);
+  log() {
+    console.log(`${this.title} - ${this.lenMin}`);
+  }
+}
 
-book.log(); // Lord Of The Rings - 1200
-book.buy(); //Buy
-
-console.log(book);
-console.log(book instanceof AudioBook1); // true
-console.log(book instanceof Book1); // true
+const book = new AudioBook('Lord Of The Rings', 'Tolkein', 20 * 60);
+book.log();
+book.buy();
