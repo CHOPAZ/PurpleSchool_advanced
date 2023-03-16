ba29d89afc87ae0e99a07c7728c87948f92e371f
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-03-16 22:22:13 +0500

    Practice - Ork strike

--- app.js
+++ app.js
@@ -3,34 +3,69 @@
 /*
   Принципы ООП в классах
 
-  Override методов - позволяет менять свойства или метода в классе, который унаследовался от др класса
+  Упражнение - 
+  Сделать класс врага со здоровьем и методом получения урона.
+  Сделать класс меча, который емеет силу и методу нанесения урона.
+  Сделать класс орка, который в 50% случаев не полцучает урон.
 */
 
-class Book {
-  constructor(title, author) {
-    this.title = title;
-    this.author = author;
+/* Класс Врага */
+class Enemy {
+  // #health;
+  health;
+  constructor(health) {
+    this.health = health;
   }
 
-  info() {
-    console.log(`${this.title} - ${this.author}`);
-  }
-}
-
-const book1 = new Book('Lord Of The Rings', 'Tolkein', 20 * 60);
-book1.info(); //Lord Of The Rings - Tolkein
-
-class EBook extends Book {
-  constructor(title, author, pages) {
-    super(title, author);
-    this.pages = pages;
-  }
-
-  /* У нас будет тоже метод info, что бы перезаписать исходный метод info. В этом заключается метод Override */
-  info() {
-    console.log(`${this.title} - ${this.author} - ${this.pages}`);
+  reciveDamage(damage) {
+    if (this.health === 0) {
+      return console.log('Enemy dead');
+    }
+    console.log(`${this.health} - ${damage} = ${this.health - damage}`);
+    this.health -= damage;
   }
 }
 
-const book2 = new EBook('Lord Of The Rings', 'Tolkein', 100);
-book2.info()
+/* Класс Меча */
+
+class Sword {
+  #damage;
+  constructor(damage) {
+    this.#damage = damage;
+  }
+  /* Использование полиморфизма */
+  strikeDamage(enemy) {
+    enemy.reciveDamage(this.#damage);
+  }
+}
+
+/* Класс Орка. Частный случай врага */
+
+class Ork extends Enemy {
+  constructor(health) {
+    super(health);
+  }
+
+  /* Столкнемся с проблемой получения урона у орка. Просто скопировать мметод получения у Enemy не получится
+  Uncaught SyntaxError: Private field '#health' must be declared in an enclosing class (at app.js:52:13)
+
+  Из-за приватного #health. Нужно убрать приватное свойство
+  */
+  reciveDamage(damage) {
+    if (this.health == 0) {
+      return console.log('Ork dead');
+    }
+    if (Math.random() > 0.5) {
+      console.log(`${this.health} - ${damage} = ${this.health - damage}`);
+      this.health -= damage;
+    }
+  }
+}
+
+const enemy = new Enemy(100);
+const sword = new Sword(10);
+sword.strikeDamage(enemy); //100 - 3 = 97
+sword.strikeDamage(enemy); //97 - 3 = 94
+
+const ork = new Ork(100);
+sword.strikeDamage(ork);
