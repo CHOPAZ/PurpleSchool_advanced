d592ff771e8bb281ee54a9a3f78e9b6e71ccfcde
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-03-18 23:53:21 +0500

    Получение данных с помощью XMLHttpRequest

--- app.js
+++ app.js
@@ -1,12 +1,43 @@
 'use strict';
 
 /*
-  JSON - JavaScript Object Notation - формат описания объектов в JS.
+  Получение данных
 
-  JSON не является оптимальным форматом данных, но является стандартом и легко читаем человека
-  {
-    "title": "iPhone 9"
-    "ключ": "значение"
-  }
+  XMLHttpRequest
 */
 
+const request = new XMLHttpRequest();
+
+/* Создали объект, наполним и отправим */
+request.open('GET', 'https://dummyjson.com/products/1');
+/* Отправляем */
+request.send();
+
+/* Подпишемся на ответ сервера */
+request.addEventListener('load', function () {
+  console.log(this.responseText); //пока просто текс
+  console.log(JSON.parse(this.responseText)); // теперь объект
+});
+
+console.log('end');
+
+/* Запросы могут приходить не последовательно */
+function req(id) {
+  const request = new XMLHttpRequest();
+  request.open('GET', 'https://dummyjson.com/products/' + id);
+  request.send();
+
+  request.addEventListener('load', function () {
+    console.log(JSON.parse(this.responseText));
+  });
+}
+
+req(''); // займет больше времени чем один объект
+req(1);
+req(2);
+req(3);
+
+/* Если необходимо соблюсти последовательность запросов, и это важно, то необходимо вручную друг за другом вставить последовательно, и есть более современные способы например async await - будет дальше расмотрено.
+
+
+*/
