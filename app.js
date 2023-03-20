f3516732f60b621d1c89a32ac30ee70363f7ea52
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-03-20 21:47:27 +0500

    Проблемы callback

--- app.js
+++ app.js
@@ -1,19 +1,40 @@
 'use strict';
 
 /*
-  Упражнение - Расчет цены
+  Проблемы callback
 
-  Получить среднюю цены 30 товаров из API
-  https://dummyjson.com/products/
+  Как правильно реализовывать последовательные запросы, и к чему они проведут в случае callback
 */
 
 const request = new XMLHttpRequest();
 request.open('GET', 'https://dummyjson.com/products/');
 request.send();
 
+/* Из практики с бэка приходит неполный список продуктов, только id. НУжно взять продукт с порядкомым новером в массие и его id и по нему уже сделать запрос, для того что бы получить о нем данных*/
+
 request.addEventListener('load', function () {
   const { products } = JSON.parse(this.responseText);
   console.log(products);
-  const sum = products.reduce((acc, product) => acc += product.price, 0);
-  console.log(sum / products.length);
+
+  /* Запрос по продукту 0 в массиве id 1 */
+  const request = new XMLHttpRequest();
+  request.open('GET', 'https://dummyjson.com/products/' + products[0].id);
+  request.send();
+  /* Подписываемся на него */
+  request.addEventListener('load', function () {
+    const data = JSON.parse(this.responseText);
+    console.log(data);
+
+    /* Запрос по продукту 1 в массиве id 2 */
+    const request = new XMLHttpRequest();
+    request.open('GET', 'https://dummyjson.com/products/' + products[1].id);
+    request.send();
+    /* Подписываемся на него */
+    request.addEventListener('load', function () {
+      const data = JSON.parse(this.responseText);
+      console.log(data);
+    });
+  });
 });
+
+/* Появляется много кода икод начинает уезжать направо - это называется callbackHall - избежать этого позволяет async await и промисы */
