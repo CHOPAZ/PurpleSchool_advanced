36d3ac26bad82839369d52a886a723fd410e8d46
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-03-20 22:02:52 +0500

    Promises and fetch

--- app.js
+++ app.js
@@ -1,40 +1,19 @@
 'use strict';
 
 /*
-  Проблемы callback
+  Promises и fetch.
 
-  Как правильно реализовывать последовательные запросы, и к чему они проведут в случае callback
+  Promises - фишка ES6, это контейнер(обертка) для значения, которое вернется в будущем.
+  Не нужно использовать callback
+  Не нужно использовать события.
+  Промис мы можем создавать сами, так и получать из Web-API
+
+  Есть разлычные состояния (жизненный цикл):
+  1.Pending - начальное состояние
+  2.Settled
+  2.1 Fulfilled - либо, все успешно
+  2.2 Rejected - либо, ошибка
 */
 
-const request = new XMLHttpRequest();
-request.open('GET', 'https://dummyjson.com/products/');
-request.send();
-
-/* Из практики с бэка приходит неполный список продуктов, только id. НУжно взять продукт с порядкомым новером в массие и его id и по нему уже сделать запрос, для того что бы получить о нем данных*/
-
-request.addEventListener('load', function () {
-  const { products } = JSON.parse(this.responseText);
-  console.log(products);
-
-  /* Запрос по продукту 0 в массиве id 1 */
-  const request = new XMLHttpRequest();
-  request.open('GET', 'https://dummyjson.com/products/' + products[0].id);
-  request.send();
-  /* Подписываемся на него */
-  request.addEventListener('load', function () {
-    const data = JSON.parse(this.responseText);
-    console.log(data);
-
-    /* Запрос по продукту 1 в массиве id 2 */
-    const request = new XMLHttpRequest();
-    request.open('GET', 'https://dummyjson.com/products/' + products[1].id);
-    request.send();
-    /* Подписываемся на него */
-    request.addEventListener('load', function () {
-      const data = JSON.parse(this.responseText);
-      console.log(data);
-    });
-  });
-});
-
-/* Появляется много кода икод начинает уезжать направо - это называется callbackHall - избежать этого позволяет async await и промисы */
+const res = fetch('https://dummyjson.com/products/1');
+console.log(res); //Promise {<pending>} - это контейнер, в который потом будет приходить ответ
