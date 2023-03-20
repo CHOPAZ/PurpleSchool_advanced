7359224b18b0e7b807346d4d3d0b7fd10b6168f8
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-03-20 21:29:16 +0500

    Practice - Price calculation

--- app.js
+++ app.js
@@ -1,43 +1,19 @@
 'use strict';
 
 /*
-  Получение данных
+  Упражнение - Расчет цены
 
-  XMLHttpRequest
+  Получить среднюю цены 30 товаров из API
+  https://dummyjson.com/products/
 */
 
 const request = new XMLHttpRequest();
-
-/* Создали объект, наполним и отправим */
-request.open('GET', 'https://dummyjson.com/products/1');
-/* Отправляем */
+request.open('GET', 'https://dummyjson.com/products/');
 request.send();
 
-/* Подпишемся на ответ сервера */
 request.addEventListener('load', function () {
-  console.log(this.responseText); //пока просто текс
-  console.log(JSON.parse(this.responseText)); // теперь объект
+  const { products } = JSON.parse(this.responseText);
+  console.log(products);
+  const sum = products.reduce((acc, product) => acc += product.price, 0);
+  console.log(sum / products.length);
 });
-
-console.log('end');
-
-/* Запросы могут приходить не последовательно */
-function req(id) {
-  const request = new XMLHttpRequest();
-  request.open('GET', 'https://dummyjson.com/products/' + id);
-  request.send();
-
-  request.addEventListener('load', function () {
-    console.log(JSON.parse(this.responseText));
-  });
-}
-
-req(''); // займет больше времени чем один объект
-req(1);
-req(2);
-req(3);
-
-/* Если необходимо соблюсти последовательность запросов, и это важно, то необходимо вручную друг за другом вставить последовательно, и есть более современные способы например async await - будет дальше расмотрено.
-
-
-*/
