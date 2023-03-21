145d93242057d8c3291c9053a46c0f88c1bd4717
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-03-21 21:07:05 +0500

    Обработка reject

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
 'use strict';
 
 /*
-  Цепочка promise
+  Обработка reject - ошибки
+*/
+
+fetch('https://dummyjson.com/productsS')
+  .then(
+    (response) => {
+      console.log(response);
+      return response.json();
+    },
+    (error) => console.log(error) // еще один аргумент для обработки ошибок. отработает если первый промис не разрезолвится ( когда ошибка в пути /dummyjsonS)
+  )
+  .then(
+    ({ products }) => {
+      console.log(products);
+      return fetch('https://dummyjson.com/products/' + products[0].id); // послали второй запрос на продукт 1
+    },
+    (error) => console.log(error) // отработает если первый промис не разрезолвится ( когда ошибка в пути /productsS)
+  )
+  .then((response) => response.json()) // обрабатываем его
+  .then((data) => {
+    console.log(data);
+  });
+
+/* Таким образом можем обрабатывать ошибки.
+  Если мы обработали одну ошибку, то мы проваливаемся в следующий then, что не очень бы и хотелось.
+
+  Чаще на практике будет применятся обработка глобальная всех цепочек ошибок: через catch - общий обработчик ошибок, в который будет проваливаться при любой ошибки из цепочки промисов
 */
 
 fetch('https://dummyjson.com/products')
@@ -16,4 +42,5 @@ fetch('https://dummyjson.com/products')
   .then((response) => response.json()) // обрабатываем его
   .then((data) => {
     console.log(data);
-  });
+  })
+  .catch((error) => console.log(error));
