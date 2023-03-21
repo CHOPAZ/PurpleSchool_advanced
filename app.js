b5fe6ab2f7ca6d96bf6f42f26ce12089bc30369d
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-03-21 20:49:56 +0500

    Цепочка promise

--- app.js
+++ app.js
@@ -1,27 +1,19 @@
 'use strict';
 
 /*
-  Обработка promises 
-
-  promises возвращает 3 доп.метода
-  1. catch - обработка ошибок
-  2. finally - всегда выполняетя в результате
-  3. then - для работы с fullfield, принимает responce , который будет получен после того как вернется запрос с данными
+  Цепочка promise
 */
 
-fetch('https://dummyjson.com/products/1')
+fetch('https://dummyjson.com/products')
   .then((response) => {
     console.log(response);
-    return response.json(); // тоже возвращает промис
+    return response.json();
   })
+  .then(({ products }) => {
+    console.log(products);
+    return fetch('https://dummyjson.com/products/' + products[0].id); // послали второй запрос на продукт 1
+  })
+  .then((response) => response.json()) // обрабатываем его
   .then((data) => {
-    console.log(data); // получили уже данные
+    console.log(data);
   });
-
-/* Упростим  запись*/
-
-const res = fetch('https://dummyjson.com/products/1')
-  .then((response) => response.json())
-  .then((data) => data);
-
-console.log(res); //получим снова  Promise {<pending>}, а не данные как выше - код является асинхронным, поэтому когда мы пытаемся получить данные и положить это res - крч дальше расскажут как правильно сделать
