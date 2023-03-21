3b9a15fa35550a3678dda283cadc34dcbc899fc6
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-03-21 21:37:52 +0500

    Practice - Select  categories

--- app.js
+++ app.js
@@ -1,25 +1,22 @@
 'use strict';
 
 /*
-  Finally - 3 случай, независимо от ошибки или выполнения запроса необходимо выполнить какое либо действие.
-  Например отобразить плашку что запрос выполнен, независимо от исхода запроса.
-  Например отправить доп. запрос на бэк для формирования метрики пользователя, например о том что он совершал этот запрос 
+  Сделать запрос на  https://dummyjson.com/products/categories,
+  получить список категорий и отобразить <select> выбора категорий.
 */
 
-fetch('https://dummyjson.com/products')
-  .then((response) => {
-    console.log(response);
-    return response.json();
-  })
-  .then(({ products }) => {
-    console.log(products);
-    return fetch('https://dummyjson.com/products/' + products[0].id);
-  })
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((error) => console.log(error))
-  .finally(() => {
-    console.log('Finnaly');
-  })
+function createSelect(arr) {
+  const el = document.querySelector('.filter');
+  el.innerHTML = `<select> 
+    ${arr.map(arrEl => `<option value=${arrEl}>${arrEl}</option>`)}  
+  </select>`
+}
+
+function getCategories() {
+  fetch('https://dummyjson.com/products/categories')
+    .then((response) => response.json())
+    .then((data) => createSelect(data))
+    .catch(error => console.error(`Ошибка: ${error}`))
+}
+
+getCategories()
