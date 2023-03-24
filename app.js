6ac588023511dde20a285b22013909790b46caaa
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-03-24 23:57:25 +0500

    Создание простого Promise

--- app.js
+++ app.js
@@ -1,23 +1,72 @@
 'use strict';
 
 /*
-  Как работает асинхронность. Как в EventLoop кладутся promise
-  Примры работы Event Loop
+  Создание простого Promise
 
-  Satack вызовов
-  WEB API
-  Callback Queue
-  Microtask Que - сюда попадают promise
+  resolve - в случае успешного выполнения вызывается колбэк
+  reject - в случае ошибки вызывается колбэк
+  P.s анологично return, но в нужный статус
+
+  Внутри можем писать логику, внешние запросы
 */
 
-console.log(1); //1
+const prom = new Promise((resolve, reject) => {
+  resolve('Secces'); // вызывается resolve, и промис прекращает свою работу
+});
 
-setTimeout(() => {
-  console.log(2); //4
-}, 0);
+/* Как нам теперь его использовать */
 
-Promise.resolve(3).then((res) => console.log(res)); //3
+prom.then((data) => console.log(data));
 
-console.log(4); //2
+/* Аналогично если хотим сделать reject по какой то логике
+  Если мы оставим так как ниже то будет ошибка Uncaught (in promise) Error, потому что нет обработки ошибки
+*/
 
-for (let i = 0; i < 10000000000000; i++) {}
+const prom1 = new Promise((resolve, reject) => {
+  if (new Date() < new Date('01/01/2024')) {
+    reject('Error'); //Uncaught (in promise) Error
+  }
+  resolve('Secces'); // вызывается resolve, и промис прекращает свою работу
+});
+
+prom1.then((data) => console.log(data));
+
+/* Теперь так же как выше, только с обработчиком ошибки */
+
+const prom2 = new Promise((resolve, reject) => {
+  if (new Date() < new Date('01/01/2024')) {
+    reject(new Error('Error'));
+  }
+  resolve('Secces');
+});
+
+prom2
+  .then((data) => console.log(data))
+  .catch((error) => console.log(error.message));
+
+/* Но пока промис не делает ничего такого длительно.
+  Правильнее использовать промисы для оборачивания длительных операций с колбэками.
+
+  Такое ручное создание промисов больше подойдет для создания удобного взаимодейсвия с calback api 
+
+  Напримере таймаута
+*/
+
+function timeout(sec) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, sec * 1000);
+  });
+}
+
+timeout(1).then(() => console.log(1));
+
+/* Таким образом setTimeout  который вызывался некрасиво, обернули в промис, и как только наш таймоут вернутся вернутся и промис. И с ожиданием можно взаимодействовать очень просто  */
+
+timeout(1).then(() => {
+  console.log(1);
+  return timeout(1).then(() => console.log(1));
+});
+
+/*  */
