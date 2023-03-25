2e73e88b28ea052debb49a6f8f46f36fb10821b5
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-03-25 15:31:17 +0500

    Статические методы Promise

--- app.js
+++ app.js
@@ -1,72 +1,33 @@
 'use strict';
 
 /*
-  Создание простого Promise
+  Статические методы Promise
 
-  resolve - в случае успешного выполнения вызывается колбэк
-  reject - в случае ошибки вызывается колбэк
-  P.s анологично return, но в нужный статус
-
-  Внутри можем писать логику, внешние запросы
+  Promise.resolve() - не  создает новый конструктор промисов, а мнговенно вызываенм resolve нашего промиса 
 */
 
-const prom = new Promise((resolve, reject) => {
-  resolve('Secces'); // вызывается resolve, и промис прекращает свою работу
-});
+Promise.resolve('Success').then((data) => console.log(data)); //получили мнгновенный ответ Success
 
-/* Как нам теперь его использовать */
+/* Сравним как будут отрабатывать промисы с течением времени */
+
+const prom = new Promise((resolve) => {
+  console.log('Constructor promise');
+
+  setTimeout(() => {
+    resolve('Timer');
+  }, 1000);
+});
 
 prom.then((data) => console.log(data));
 
-/* Аналогично если хотим сделать reject по какой то логике
-  Если мы оставим так как ниже то будет ошибка Uncaught (in promise) Error, потому что нет обработки ошибки
+Promise.resolve('Instant').then((data) => console.log(data));
+
+Promise.reject(new Error('Error')).catch((error) => console.error(error))
+
+/* Вывод будет следующим:
+  Constructor promise
+  Success
+  Instant
+  Timer
+  Error
 */
-
-const prom1 = new Promise((resolve, reject) => {
-  if (new Date() < new Date('01/01/2024')) {
-    reject('Error'); //Uncaught (in promise) Error
-  }
-  resolve('Secces'); // вызывается resolve, и промис прекращает свою работу
-});
-
-prom1.then((data) => console.log(data));
-
-/* Теперь так же как выше, только с обработчиком ошибки */
-
-const prom2 = new Promise((resolve, reject) => {
-  if (new Date() < new Date('01/01/2024')) {
-    reject(new Error('Error'));
-  }
-  resolve('Secces');
-});
-
-prom2
-  .then((data) => console.log(data))
-  .catch((error) => console.log(error.message));
-
-/* Но пока промис не делает ничего такого длительно.
-  Правильнее использовать промисы для оборачивания длительных операций с колбэками.
-
-  Такое ручное создание промисов больше подойдет для создания удобного взаимодейсвия с calback api 
-
-  Напримере таймаута
-*/
-
-function timeout(sec) {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve();
-    }, sec * 1000);
-  });
-}
-
-timeout(1).then(() => console.log(1));
-
-/* Таким образом setTimeout  который вызывался некрасиво, обернули в промис, и как только наш таймоут вернутся вернутся и промис. И с ожиданием можно взаимодействовать очень просто  */
-
-timeout(1).then(() => {
-  console.log(1);
-  return timeout(1).then(() => console.log(1));
-});
-
-/*  */
