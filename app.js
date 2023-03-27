b13289110cc4a7b03a04c3da1b43c1f05c11bf65
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-03-27 22:14:57 +0500

    Последовательность выполнения

--- app.js
+++ app.js
@@ -1,26 +1,49 @@
 'use strict';
 
 /*
-  Асинхронные методы
+  Последовательность выполнения на верхнем уровне
+  Например:
+  1. Сделать действие
+  2. Получить продукты.
+  3. Выполнить действие
+
+  Способ: Immediately Invoked Function Expression - IIFE
 */
-
-class ProductRepository {
-  async getProduct() {
-    const responce = await fetch('https://dummyjson.com/products');
-    console.log(await responce.json());
-  }
-}
-
-const prd = new ProductRepository();
-prd.getProduct();
-
-
 
 /* Асинхронные стрелочные функции */
 
 const asyncArrow = async () => {
   const responce = await fetch('https://dummyjson.com/products');
-  console.log(await responce.json());
+  const data = await responce.json();
+  return data;
 };
-asyncArrow();
 
+/* Не правильно */
+console.log('1');
+asyncArrow()
+  .then((data) => {
+    console.log(data);
+  })
+  .finally(() => console.log('3'));
+
+/* Правильная запись */
+(async () => {
+  console.log(1);
+  const res = await asyncArrow()
+  console.log(res);
+  console.log(2);
+})();
+
+/* P.S В случае использования try catch нужно перебросить ошибку дальше */
+
+const asyncArrow2 = async () => {
+  try {
+    //code
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+asyncArrow2()
+  .then()
+  .catch((error) => console.log(error));
