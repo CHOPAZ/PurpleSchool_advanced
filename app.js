5b61d6fe5678419a5dadf0958031f645f1c5ccb8
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-03-27 21:50:37 +0500

    Practice - get my City

--- app.js
+++ app.js
@@ -1,87 +1,43 @@
 'use strict';
 
 /*
-  try catch
+  Получить геолокацию пользователя через
+	Geolocation.getCurrentPosition() (WEB API)
+	и по координатам определить город, отправив запрос:
+	https://api.bigdatacloud.net/data/reverse-geocode-client?
+	latitude=00&longitude=00
 */
 
-function getProducts() {
-  fetch('https://dummyjson.com/products')
-    .then((respons) => respons.json())
-    .then((data) => console.log(data));
-}
-getProducts();
-console.log('end');
-
-/* Перепишем на async await
-
-await говорит что: -Подождать пока отработает fetch запрос, и положить его в res
-*/
-
-/* Функция по умолчанию возращает промис */
-async function getProducts1() {
-  const res = await fetch('https://dummyjson.com/products');
-  const data = await res.json();
-  console.log(data);
-}
-
-getProducts1();
-
-/* Несколько запросов */
-
-async function getProducts2() {
-  const productsResponce = await fetch('https://dummyjson.com/products');
-  const { products } = await productsResponce.json();
-  console.log(products);
-
-  const productResponce = await fetch(
-    'https://dummyjson.com/products/' + products[0].id
-  );
-  const product = await productResponce.json();
-  console.log(product);
-}
-
-getProducts2();
-
-/* ---------------------------------------------------------------------- */
-
-/* Обработка ошибок через try catch
-  try - попробуй выполнить код
-  catch - обработка ошибки 
-*/
-
-async function getProducts3() {
-  try {
-    const productsResponce = await fetch('https://dummyjson.com/productss');
-    if (!productsResponce.ok) {
-      throw new Error(`ERROR ${productsResponce.status}`)
-    }
-    const { products } = await productsResponce.json();
-
-    const productResponce = await fetch(
-      'https://dummyjson.com/products/' + products[0].id
+function getMyCoordinates() {
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(
+      ({ coords }) => {
+        resolve({
+          latitude: coords.latitude,
+          longitude: coords.longitude,
+        });
+      },
+      (err) => {
+        reject(err);
+      }
     );
-    const product = await productResponce.json();
+  });
+}
+
+async function getMyCity() {
+  try {
+    const { latitude, longitude } = await getMyCoordinates();
+    const responce =
+      await fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?
+    latitude=${latitude}&longitude=${longitude}`);
+    if (!responce.ok) {
+      throw new Error(responce.status);
+    }
+    const data = await responce.json();
+    console.log(data);
   } catch (error) {
-    console.error(error.message)
-  } finally {
-    console.log('Finally');
+    console.err(error);
   }
 }
 
-getProducts3();
-
-
-/* try catch работает не только с промисами */
-
-try {
-  const a = 5;
-  a = 6;
-} catch (error) {
-  console.error(error);
-}
-
-try {
-  JSON.parse('{d')
-} catch (error) {
-  console.error(error);
-}
+getMyCity();
