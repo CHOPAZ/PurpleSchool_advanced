320b33240980145a348ac6098161507969f29a2b
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-03-27 21:51:29 +0500

    Асинхронные методы

--- app.js
+++ app.js
@@ -1,43 +1,26 @@
 'use strict';
 
 /*
-  Получить геолокацию пользователя через
-	Geolocation.getCurrentPosition() (WEB API)
-	и по координатам определить город, отправив запрос:
-	https://api.bigdatacloud.net/data/reverse-geocode-client?
-	latitude=00&longitude=00
+  Асинхронные методы
 */
 
-function getMyCoordinates() {
-  return new Promise((resolve, reject) => {
-    navigator.geolocation.getCurrentPosition(
-      ({ coords }) => {
-        resolve({
-          latitude: coords.latitude,
-          longitude: coords.longitude,
-        });
-      },
-      (err) => {
-        reject(err);
-      }
-    );
-  });
-}
-
-async function getMyCity() {
-  try {
-    const { latitude, longitude } = await getMyCoordinates();
-    const responce =
-      await fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?
-    latitude=${latitude}&longitude=${longitude}`);
-    if (!responce.ok) {
-      throw new Error(responce.status);
-    }
-    const data = await responce.json();
-    console.log(data);
-  } catch (error) {
-    console.err(error);
+class ProductRepository {
+  async getProduct() {
+    const responce = await fetch('https://dummyjson.com/products');
+    console.log(await responce.json());
   }
 }
 
-getMyCity();
+const prd = new ProductRepository();
+prd.getProduct();
+
+
+
+/* Асинхронные стрелочные функции */
+
+const asyncArrow = async () => {
+  const responce = await fetch('https://dummyjson.com/products');
+  console.log(await responce.json());
+};
+asyncArrow();
+
