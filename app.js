85b4ea47e63f18f5a9b975ac913ec504727357e2
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-03-28 20:26:22 +0500

    Визуальное положение элементов

--- app.js
+++ app.js
@@ -1,50 +1,27 @@
 'use strict';
 
 /*
-  Как работает DOM более детально
-  JS - DOM API (Управление HTML документом) - Browser
-
-  Рабоа с элементами
+  Визуальное положение элементов
 */
 
-console.log(document);
-console.log(document.documentElement);
-console.log(document.head);
-console.log(document.body);
+function generate(event) {
+  console.log(`X offset ${window.pageXOffset}`);
+  console.log(`Y offset ${window.pageYOffset}`);
+  console.log(`clientWidth: ${document.documentElement.clientWidth}`);
+  console.log(`clientHeigth: ${document.documentElement.clientHeight}`);
+  /* Где распологается конкретный элемент */
+  console.log(event.target.getBoundingClientRect()); //выведет все параметры расположения элемента кнопки относительно нашего view-ort
+  console.log(event.target);
 
-const el = document.querySelector('.wrapper');
-const el2 = document.querySelectorAll('meta');
-console.log(el);
-console.log(el2);
+  /* ИНфо о расположении элемента down */
+  const el = document.querySelector('.down');
+  const rectDown = el.getBoundingClientRect()
 
-const el3 = document.getElementsByClassName('wrapper');
-const el4 = document.getElementsByTagName('meta');
-console.log(el3);
-console.log(el4);
 
-const btn = document.createElement('button');
-const btn2 = document.createElement('button');
-const btn3 = document.createElement('button');
-const btn4 = document.createElement('button');
-btn.innerHTML = 'BTN-Append';
-btn2.innerHTML = 'BTN-Prepend';
-btn3.innerHTML = 'BTN-Befor';
-btn4.innerHTML = 'BTN-After';
-
-/* Добавить вконец wrapepr */
-el.append(btn);
-
-/* Добавить вначало wrapper */
-el.prepend(btn2);
-
-/* Добавление перед wrapper */
-el.before(btn3);
-
-/* Добавление после wraper */
-el.after(btn4);
-
-/* Удаление элемента */
-
-function generate() {
-  el.remove();
+  /* Скролинг. При нажатии на кнопку генерации  доскролить до нужного элемента*/
+  window.scrollTo({
+    left: window.pageXOffset + rectDown.left,
+    top: window.pageYOffset + rectDown.top,
+    behavior: 'smooth' // плавный скрол до элемента
+  })
 }
