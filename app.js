e90ca5026342159223983689e1e046b7775c685a
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-03-30 21:32:40 +0500

    Обзор npm

--- app.js
+++ app.js
@@ -1,33 +1,16 @@
 'use strict';
 
 /*
-  Процесс сборки
+  Обзор npm - node package manager
+  
+  Пакетные менеджеры NodeJS
+  1. npm - консольная утитила, которая обращается в большой регистры информации с api, в котором хранятся пакеты
+  2. yarn - от facebook
+  3. pnpm -  координально отличается от npm и yarn. ЗАвисимости централизованы и использует линки этих зависимостей в приложении.
+  Например если используется реакт одной версии в разных проектах, то pnpm будет ссылкаться один реакт. Быстрая установка зависимостей
+  
 
-  app.js     \
-  calc.js    -> Сборка -> Транспиляция / полифилы -> Bundle
-  Пакеты npm |
+  NPM Cache - используется для быстрой установки зависимости. Когда утсанавливается пакет, npm идет сначала в кэш - этот кэш в последующем используется для быстрой востановления зависимостей
 
-  Сборщики:
-  WebPack
-  Vite
-  Parsin
-  RollUp
-
-  Будем использовать rollup - у него есть свойство zero config
-
-  1. rollup app.js --file bundle.js --format iife  - приобразует все к iife - файл bundle.js
-
-  2.rollup app.js --file bundle1.js --format cjs - commonjs - bundle2.js
-
-  3. rollup app.js --file bundle2.js --format umd --name "myBundle" - universety bundle - для старых браузеров старые форматы, для новыйх модульности
+  чистка кэша - npm cache clean
 */
-
-import { add, sub,} from './calc.js';
-
-
-
-console.log(sub(1, 3));
-console.log(add(1, 3));
-
-
-
